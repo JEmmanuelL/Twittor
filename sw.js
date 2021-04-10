69b9db30aa32e6c5318225b41743de7f79000ad8
JEmmanuelLJ <Emma_lopez2917@hotmail.com>
2021-04-09 19:11:00 -0500

    modificaciones de iphone

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE    = 'static-v3';
-const DYNAMIC_CACHE   = 'dynamic-v1';
+const STATIC_CACHE    = 'static-v4';
+const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -44,8 +44,12 @@ self.addEventListener('activate', e=> {
     const respuesta = caches.keys().then(keys =>{
 
         keys.forEach(key => {
-            //static-v4
+            //para borrar las versiones anteriores del static cache
             if(key !== STATIC_CACHE && key.includes('static')){
+                return caches.delete(key);
+            }
+            //para borrar las versiones anteriores del dynamic cache
+            if(key !== DYNAMIC_CACHE && key.includes('dynamic')){
                 return caches.delete(key);
             }
 
