2ab631b7961c264cc4ffef11618b403db1807917
JEmmanuelLJ <Emma_lopez2917@hotmail.com>
2021-04-09 19:47:12 -0500

    modificacion final para verificar error en index

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 //imports ---esto importa/ hace la referencia al archivo utils para que se puedan comunicar entre ellos
 importScripts('js/sw-utils.js');
+//version 1
 
-
-const STATIC_CACHE    = 'static-v4';
-const DYNAMIC_CACHE   = 'dynamic-v2';
+const STATIC_CACHE    = 'static-v1';
+const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
